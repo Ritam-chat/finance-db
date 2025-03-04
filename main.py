a5f4003722fc78e7fbeaa44c8b9ea676c1917a93
Ritam Chatterjee <ritam2021@gmail.com>
2025-03-04 23:09:35 +0530

    Bug fixes & Passkey

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit
 
 from UI import *
 import json
@@ -14,8 +15,6 @@ st.set_page_config(
 
 updates = False
 cookies = CookieController()
-
-
 with open('info.json','r') as f:
     data = json.load(f)
     # URL = "https://python-api-finance-dashboards.onrender.com/records?uname=ritariya&key=210102"
@@ -23,8 +22,12 @@ with open('info.json','r') as f:
     # URL = "http://127.0.0.1:5000/records?uname=ritariya&key=210102"
     if cookies.get('finance-user') is None:
         x = st.selectbox(label='Enter you Name : ',options=['Ritam','Riya'],index=None)
-        if x:
-            cookies.set('finance-user',x)
+        y = st.text_input(label='Enter your specific Key : ',type='password')
+        if x and y:
+            if y == 'LOVEYOUBABU':
+                cookies.set('finance-user',x)
+            else:
+                st.error('Please enter your key!')
             try:
                 cookies.refresh()
             except Exception as e:
@@ -44,8 +47,8 @@ with open('info.json','r') as f:
                 datetime.strptime(data[user]['LastUpdateDate'], "%d-%b-%Y %p").day == datetime.now().day and
                 data[user]['LastUpdateDate'][-2] != datetime.now().strftime('%p')[-2]
         ) or cookies.get('reload') == True:
-
-            cookies.remove('reload')
+            if cookies.get('reload'):
+                cookies.remove('reload')
             updates = True
             paymentInfo = requests.get(URL,headers={'USER':user}).json()
             data[user]['LastUpdateDate'] = str(datetime.now().strftime("%d-%b-%Y %p"))
